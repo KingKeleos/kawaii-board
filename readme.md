2160cf52ab2378cc97957d7a055c88ab9097a9f0
kingkeleos <kranzan99@gmail.com>
2023-07-12 21:37:39 +0200

    init

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Kawaii-Board
+
+This app will play UwU-Sounds whenever you press a button on the keyboard.
+In the current state, only one sound is played and the sound files are fixed and not generated during the process.
+
+## Usage
---
+++ sound/sound.py
@@ -0,0 +1,20 @@
+import winsound
+
+def play():
+    filename='media/UWU.wav'
+    winsound.PlaySound(filename, winsound.SND_FILENAME)
+
+def pitchlow():
+    winsound.PlaySound('media/UWU-low.wav', winsound.SND_FILENAME)
+
+def pitchhighest():
+    winsound.PlaySound('media/UWU-Highest.wav', winsound.SND_FILENAME)
+
+def pitchhigh():
+    winsound.PlaySound('media/UWU-High.wav', winsound.SND_FILENAME)
+
+def pitchlow():
+    winsound.PlaySound('media/UWU-low.wav', winsound.SND_FILENAME)
+
+def pitch_enter():
+    winsound.PlaySound('media/UWU-Enter.wav', winsound.SND_FILENAME)
